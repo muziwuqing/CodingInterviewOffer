699b15f72f06e94068575e7adf4cf06822d45dfd
吴林哲 <linzhe@yunshan.net>
2021-08-05 17:52:44 +0800

    修改Bug

--- 工作/linzhetest.sh
+++ 工作/linzhetest.sh
@@ -1,40 +1,60 @@
+#!/bin/bash
 echo '------------test-----------'
 
 blkid>linzhetest.log
 
 FILE=linzhetest.log
 MATCH=KOLLA_CEPH_DATA_BS_
+sum=0
+sumM=0
+flag=0
+
 
 while read line
 do
     result=`echo $line | grep PARTLABEL=\"$MATCH.*`
     if [[ "$result" != "" ]]
     then
+        ((sum++))
         match=`echo $result | grep PARTLABEL=\"$MATCH._B`
         if [[ "$match" != "" ]]
         then
-            echo '-------------------------------------------------------'
-            if [[ $result == *"UUID"* ]];
+            if [[ $result == *" UUID"* ]];
             then
+                ((sumM++))
+                flag=1
+                echo -e '\n'
+                echo $result
                 echo 'ERROR, It is can not mount';
-            else
-                echo 'It is OK';
             fi
-            echo $result
-            echo '--------------------------------------------------------'
         else
-            echo '--------------------------------------------------------'
-            if [[ $result == *"UUID"* ]];
+            if [[ $result != *" UUID"* ]];
             then
-                echo 'It is OK';
+                flag=1
+                echo -e '\n'
+                echo $result
+                echo 'ERROR, It is not mounted';
             else
-                echo 'It is not mounted';
-
+                ((sumM++))
             fi
-            echo $result
-            echo '--------------------------------------------------------'
         fi
     fi
 done < $FILE
 
+if [[ $flag == "0" ]];
+then
+    echo -e '\n'
+    echo 'All is OK';
+fi
+
+
+
+echo -e '\n'
+echo -n 'The total amount of the kolla disk is : '
+echo $sum
+echo -e '\n'
+echo -n 'The amount of the kolla disk which has been mounted is : '
+echo $sumM
+
+echo -e '\n'
 echo '----------end--test--------'
